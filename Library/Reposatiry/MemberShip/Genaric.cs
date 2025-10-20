3614aeeca9252d02e5eb2972030059804c979a55
mohamed.ahmed <mohamed.ahmed@SIS-SR-MOHAMEDS>
2025-10-20 08:32:03 +0300

    jjjj

--- Library/Reposatiry/MemberShip/Genaric.cs
+++ Library/Reposatiry/MemberShip/Genaric.cs
@@ -38,7 +38,7 @@ namespace Library.Reposatiry.MemberShip
 
         public async Task SaveChangesAsync() //////////////////////////////////why _context not _dbset
         {
-             await _context.SaveChangesAsync();jgljqs
+             await _context.SaveChangesAsync();jgljqshbjhjk
         }//ljpo
 
         public async Task UpdateAsync(T entity)
